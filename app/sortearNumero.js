b2e73b368be7ecbc4279c526e510c6fc5eceb420
Everton Ferreira Lima <60532638+evertonflima04@users.noreply.github.com>
2023-02-27 00:21:46 -0300

    Finalizando o jogo de sortear um número secreto

---
+++ app/sortearNumero.js
@@ -0,0 +1,15 @@
+const menorValor = 1
+const maiorValor = 1000
+const elementoMenorValor = document.getElementById('menor-valor')
+const elementoMaiorValor = document.getElementById('maior-valor')
+const numeroSecreto = gerarNumeroAleatorio()
+
+elementoMenorValor.innerHTML = menorValor
+elementoMaiorValor.innerHTML = maiorValor
+
+
+function gerarNumeroAleatorio(){
+    return parseInt(Math.random() * maiorValor + 1)
+}
+
+console.log(numeroSecreto)
